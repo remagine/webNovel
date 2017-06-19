a8fc5839efe28c74d6a67146feb113bf1e3199c8
arthur <arthur@iropke.com>
2017-06-19 18:25:37 +0900

    Story 엔티티 구조 변경

--- src/main/java/com/arthur/webnovel/entity/Story.java
+++ src/main/java/com/arthur/webnovel/entity/Story.java
@@ -1,18 +1,30 @@
 package com.arthur.webnovel.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.arthur.webnovel.code.State;
 
 @Entity
 @Table(name = "story")
 public class Story {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_story")
+    @SequenceGenerator(name = "seq_story", sequenceName = "story_id_seq", allocationSize = 1)
     private Integer id;
 
     @Column(name = "title")
@@ -29,6 +41,27 @@ public class Story {
 
     @Column(name = "tag")
     private String tag;
+
+    @Column(name = "co_author")
+    private String coAuthor;
+
+    @Column(name = "cover_image")
+    private String coverImage;
+
+    @Column(name = "views")
+    private Integer views;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    private State state;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_at", insertable = false, updatable = false)
+    private Date createAt;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
 
     public Integer getId() {
         return id;
@@ -78,4 +111,51 @@ public class Story {
         this.tag = tag;
     }
 
+    public String getCoAuthor() {
+        return coAuthor;
+    }
+
+    public void setCoAuthor(String coAuthor) {
+        this.coAuthor = coAuthor;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
